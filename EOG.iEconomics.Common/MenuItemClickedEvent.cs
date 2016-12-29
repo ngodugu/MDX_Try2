1ded3df8bac531705b622984438c4fa3f2d47b1d
Neelima Godugu <ngodugu@eogresources.com>
2016-12-29 10:14:44 -0600

    first commit

---
+++ EOG.iEconomics.Common/MenuItemClickedEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EOG.iEconomics.Common
+{
+    public class MenuItemClickedEvent:CompositeWpfEvent<>
+    {
+    }
+
+    public class MenutItemClickedEventPayload
+    {
+        public string MenuItemId { get; set; }
+    }
+}
